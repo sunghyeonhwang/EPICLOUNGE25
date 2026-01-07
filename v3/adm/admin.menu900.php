8570611b63b968d2f421ac2ddd092920ce19ea88
sunghyeon <sunghyeon.hwang@griff.co.kr>
2026-01-07 22:44:14 +0900

    관리자 메뉴 이름을 주소변환기로 변경

--- v3/adm/admin.menu900.php
+++ v3/adm/admin.menu900.php
@@ -1,6 +1,6 @@
 <?php
 $menu["menu900"] = array (
     array('900000', 'SERVICE', G5_ADMIN_URL.'/service_main.php', 'service'),
-    array('900100', '서비스 메인', G5_ADMIN_URL.'/service_main.php', 'service_info')
+    array('900100', '주소변환기', G5_ADMIN_URL.'/service_main.php', 'service_info')
 );
 ?>
