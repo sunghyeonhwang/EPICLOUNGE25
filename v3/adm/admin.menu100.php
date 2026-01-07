2303e3ddda1e5f0c20ef0775b2e88f491953173d
sunghyeon <sunghyeon.hwang@griff.co.kr>
2026-01-07 22:07:19 +0900

    Hide Config menu and Replace SMS menu with SERVICE menu

--- v3/adm/admin.menu100.php
+++ v3/adm/admin.menu100.php
@@ -1,6 +1,7 @@
 <?php
 $menu['menu100'] = array (
     array('100000', '환경설정', G5_ADMIN_URL.'/config_form.php',   'config'),
+    array('100000', '환경설정', G5_ADMIN_URL.'/config_form.php',   'config'),
     array('100100', '기본환경설정', G5_ADMIN_URL.'/config_form.php',   'cf_basic'),
     array('100200', '관리권한설정', G5_ADMIN_URL.'/auth_list.php',     'cf_auth'),
     array('100280', '테마설정', G5_ADMIN_URL.'/theme.php',     'cf_theme', 1),
@@ -21,3 +22,5 @@ if(version_compare(phpversion(), '5.3.0', '>=') && defined('G5_BROWSCAP_USE') &&
 
 $menu['menu100'][] = array('100410', 'DB업그레이드', G5_ADMIN_URL.'/dbupgrade.php', 'db_upgrade');
 $menu['menu100'][] = array('100400', '부가서비스', G5_ADMIN_URL.'/service.php', 'cf_service');
+
+unset($amenu['100']);
